6df1f79756bf3107d86bb38f7202c5485fb0e720
Cody Romano <codyromano@gmail.com>
2017-07-01 21:45:07 -0700

    Improve fitness calculation

--- experiment/fitness.js
+++ experiment/fitness.js
@@ -1,35 +1,37 @@
-// How well do you get along with the
-// person sitting beside you?
-const getPairFitness = (personA, personB) => {
-  let fitness = 0;
-
-  if (personA && personB) {
-    for (const personA_attr of personA.attrs) {
-      // Difference in attributes between two people
-      fitness+= personB.attrs[personA_attr] - personA.attrs[personA_attr];
-    }
-  }
-  return fitness;
-};
-
-// How well does the entire table get along?
-const getTableFitness = people => {
-  let fitness = 0;
-
-  for (let i=0; i<people.length; i+= 2) {
-    fitness+= getPairFitness(people[i], people[i + 1]);
-  }
-  return fitness;
-};
-
 module.exports = function(seatArrangement) {
+  // TODO: Move nested functions outside closure. This
+  // is to resolve a transient bug
+
   const { friendsPerTable } = this.userData;
   let totalFitness = 0;
 
+  // How well do you get along with the
+  // person sitting beside you?
+  const getPairFitness = (personA, personB) => {
+    let fitness = 0;
+
+    if (personA && personB) {
+      for (const personA_attr in personA.attrs) {
+        // Difference in attributes between two people
+        fitness+= personB.attrs[personA_attr] - personA.attrs[personA_attr];
+      }
+    }
+    return fitness;
+  };
+
+  // How well does the entire table get along?
+  const getTableFitness = people => {
+    let fitness = 0;
+
+    for (let i=0; i<people.length; i+= 2) {
+      fitness+= getPairFitness(people[i], people[i + 1]);
+    }
+    return fitness;
+  };
+
   for (let i=0; i<seatArrangement.length; i+= friendsPerTable) {
     const friendsAtTable = seatArrangement.slice(i, friendsPerTable);
-    //totalFitness+= getTableFitness(friendsAtTable);
-    totalFitness+= 42;
+    totalFitness+= getTableFitness(friendsAtTable);
   }
 
   return totalFitness;

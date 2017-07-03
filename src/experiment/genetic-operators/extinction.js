e2764afb6c14cd9bb6148a6015e24b2b6fa0a454
Cody Romano <codyromano@gmail.com>
2017-07-03 12:51:01 -0700

    Fix extinction function

--- src/experiment/genetic-operators/extinction.js
+++ src/experiment/genetic-operators/extinction.js
@@ -4,5 +4,18 @@ import sortByWeakness from 'experiment/helpers/sortByWeakness';
 * @returns {Array} Two weakest genotypes
 */
 export default function(genotypes) {
-  return genotypes.sort(sortByWeakness).slice(0, 2);
-};
+  // TODO: Room for optimization here.
+  // We can splice weakest with one iteration
+  const weakIDs = genotypes
+    .sort(sortByWeakness)
+    .slice(0, 2)
+    .map(item => item.id);
+
+  genotypes.forEach((genotype, i, array) => {
+    if (weakIDs.includes(genotype.id)) {
+      array.splice(i, 1);
+    } 
+  });
+
+  return genotypes;
+}

bd4e4375270d66dad8597708a10e6bb7047b1752
Cody Romano <codyromano@gmail.com>
2017-07-03 23:56:04 -0700

    Experiment page

---
+++ src/components/mutable/ExperimentPage/index.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'; // eslint-disable-line
+import { onExperimentReady } from 'experiment/GeneticExperimentFactory';
+import Friend from 'components/Friend';
+import styles from 'components/mutable/ExperimentPage/experimentPage.css';
+
+export default class ExperimentPage extends Component {
+  constructor() {
+    super();
+    this.state = {
+      friends: []
+    };
+  }
+  componentDidMount() {
+    onExperimentReady(friends => {
+      this.setState({friends});
+    });
+  }
+  render() {
+    const friends = this.state.friends.map((friendData, i) => (
+      <Friend {...friendData} key={i}/>
+    ));
+
+    return (
+      <div>
+        <div className={styles.mosaic}>{friends}</div>
+      </div>
+    );
+  }
+}
